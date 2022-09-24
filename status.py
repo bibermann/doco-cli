39660328a3dbb0a63a4b3e22a13b595c26968d47
Fabian Sandoval Saldias <fabianvss@gmail.com>
2022-09-24 12:51:58 +0200

    Progress

--- status.py
+++ status.py
@@ -48,7 +48,7 @@ def load_compose_ps(cwd: str, file: str):
     return json.loads(result.stdout)
 
 
-def colored_container_state(state: str):
+def colored_container_state(state: str) -> str:
     if state == 'running':
         return f'[b green]{state}[/]'
     elif state == 'exited' or state == 'dead':
@@ -57,34 +57,35 @@ def colored_container_state(state: str):
         return f'[b yellow]{state}[/]'
 
 
-def colored_path(path: str):
+def dim_path(path: str, dimmed_prefix: str, bold: bool = False) -> str:
+    if path.startswith(dimmed_prefix):
+        return f"[dim]{dimmed_prefix}[/]{'[b]' if bold else ''}{path[len(dimmed_prefix):]}{'[/]' if bold else ''}"
+    else:
+        return path
+
+
+def colored_path(path: str, dimmed_prefix: t.Optional[str] = None) -> str:
     formatted = re.sub(r'^/disk/([^/]+)/volumes/([^/]+)(.*)$',
                        r'[dim]/disk/[/][b]\1[/][dim]/volumes/[/][b]\2[/]\3', path)
     if formatted != path:
         return formatted
-    return f'[b]{path}[/]'
+    return dim_path(path, dimmed_prefix=dimmed_prefix,
+                    bold=True) if dimmed_prefix is not None else f"[b]{path}[/]"
 
 
-def colored_readonly(text: str, ro: bool, is_volume: bool):
+def colored_readonly(text: str, ro: bool, is_volume: bool) -> str:
     ro_text = text if ro else f'[dark_orange]{text}[/]'
     return ro_text if not is_volume else f'[dim]{ro_text}[/]'
 
 
-def colored_image(image: str):
+def colored_image(image: str) -> str:
     if not ':' in image:
         image += ':latest'
     return re.sub(r'^(docker.io/)?([^:]*):(.*)', r'[bright_blue][dim]\1[/][b]\2[/][dim]:[/]\3[/]', image)
 
 
-def dim_path(path: str, dimmed_prefix: str):
-    if path.startswith(dimmed_prefix):
-        return f'[dim]{dimmed_prefix}[/]{path[len(dimmed_prefix):]}'
-    else:
-        return path
-
-
-def colored_dockerfile(dockerfile: str, dimmed_prefix: str):
-    formatted = dim_path(dockerfile, dimmed_prefix)
+def colored_dockerfile(dockerfile: str, dimmed_prefix: str) -> str:
+    formatted = dim_path(dockerfile, dimmed_prefix=dimmed_prefix)
     formatted = re.sub(r'^(.*)Dockerfile(.*)$',
                        r'[bright_blue]\1[dim]Dockerfile[/]\2[/]', formatted)
     if formatted != dockerfile:
@@ -92,18 +93,32 @@ def colored_dockerfile(dockerfile: str, dimmed_prefix: str):
     return f'[bright_blue]{dockerfile}[/]'
 
 
-def relative_path_if_below(path: str):
+def colored_key_value(text: str, key: str, value: str) -> str:
+    if value.lower() in ['true', 'yes', 'on']:
+        return f"[green]{text}[/]"
+    elif value.lower() in ['false', 'no', 'off']:
+        return f"[red]{text}[/]"
+    elif value.isnumeric():
+        return f"[bright_blue]{text}[/]"
+    elif re.search(r'(?:\b|_)(?:pw|pass|password|token|secret)(?:\b|_)', key.lower()):
+        return f"[dim dark_orange]{text}[/]"
+    else:
+        return text.replace(',', '[b white],[/]')
+
+
+def relative_path_if_below(path: str) -> str:
+    is_dir = os.path.isdir(path)
     relpath = os.path.relpath(path)
     if relpath.startswith('../'):
-        return os.path.abspath(path)
+        return os.path.abspath(path) + ('/' if is_dir else '')
     else:
         if not relpath == '.' and not relpath.startswith('./') and not relpath.startswith('/'):
-            return './' + relpath
+            return './' + relpath + ('/' if is_dir and relpath != '' else '')
         else:
             return relpath
 
 
-def colored_port_mapping(mapping: t.Tuple[str, str]):
+def colored_port_mapping(mapping: t.Tuple[str, str]) -> str:
     return f'{mapping[0]}[dim]:{mapping[1]}[/]'
 
 
@@ -157,12 +172,38 @@ def print_project(project: str, print_path: bool, output_build: bool, list_volum
         if 'ports' in service:
             ports = ' '.join(colored_port_mapping((p['published'], p['target'])) for p in service['ports'])
 
-        s = tree.add(
-            f"[b]{service_name}[/] {ports + ' ' if ports != '' else ''}{colored_container_state(state)} {source}")
+        service_line = [
+            colored_container_state(state),
+            f"[b]{service_name}[/]",
+            ports,
+            source,
+        ]
+        s = tree.add(' '.join(z for z in service_line if z != ''))
 
         if output_build and not is_image:
-            build_context = dim_path(relative_path_if_below(service['build']['context']), compose_dir)
+            build_context = dim_path(relative_path_if_below(service['build']['context']),
+                                     dimmed_prefix=compose_dir)
             s.add(f'[i]Build context:[/] {build_context}')
+
+        if output_build and not is_image and 'args' in service['build']:
+            table = rich.table.Table(title_justify='left', style='dim', header_style='i')
+            s.add(table)
+            table.add_column("Build argument")
+            table.add_column("Value")
+            for arg, value in service['build']['args'].items():
+                table.add_row(
+                    colored_key_value(arg, key=arg, value=value) if value != '' else f'[dim]{arg}[/]',
+                    colored_key_value(value, key=arg, value=value))
+
+        if list_environment and 'environment' in service:
+            table = rich.table.Table(title_justify='left', style='dim', header_style='i')
+            s.add(table)
+            table.add_column("Environment variable")
+            table.add_column("Value")
+            for env, value in service['environment'].items():
+                table.add_row(
+                    colored_key_value(env, key=env, value=value) if value != '' else f'[dim]{env}[/]',
+                    colored_key_value(value, key=env, value=value))
 
         if list_volumes > 0 and 'volumes' in service:
             table = rich.table.Table(title_justify='left', style='dim', header_style='i')
@@ -174,8 +215,8 @@ def print_project(project: str, print_path: bool, output_build: bool, list_volum
                 ro = volume.get('read_only', False)
                 is_volume = volume['type'] == 'volume'
                 source_volume = volume['source'] if is_volume else \
-                    colored_path(relative_path_if_below(volume['source']))
-                files = rich.tree.Tree(colored_readonly(colored_path(source_volume), ro, is_volume))
+                    colored_path(relative_path_if_below(volume['source']), dimmed_prefix=compose_dir)
+                files = rich.tree.Tree(colored_readonly(source_volume, ro, is_volume))
                 if list_volumes > 1 and volume['source'].startswith('/') and os.path.isdir(volume['source']):
                     for f in sorted(os.listdir(volume['source'])):
                         if os.path.isdir(os.path.join(volume['source'], f)):
@@ -186,22 +227,6 @@ def print_project(project: str, print_path: bool, output_build: bool, list_volum
                               colored_readonly(volume['target'], ro, is_volume),
                               colored_readonly('ro' if ro else 'rw', ro, is_volume))
 
-        if output_build and not is_image and 'args' in service['build']:
-            table = rich.table.Table(title_justify='left', style='dim', header_style='i')
-            s.add(table)
-            table.add_column("Build argument")
-            table.add_column("Value")
-            for arg, value in service['build']['args'].items():
-                table.add_row(arg if value != '' else f'[dim]{arg}[/]', value)
-
-        if list_environment and 'environment' in service:
-            table = rich.table.Table(title_justify='left', style='dim', header_style='i')
-            s.add(table)
-            table.add_column("Environment variable")
-            table.add_column("Value")
-            for env, value in service['environment'].items():
-                table.add_row(env if value != '' else f'[dim]{env}[/]', value)
-
     rich.print(tree)
 
 
@@ -209,18 +234,20 @@ def main() -> int:
     parser = argparse.ArgumentParser(prog='PROG')
     parser.add_argument('projects', nargs='*', default=['.'],
                         help='compose files and/or directories containing a docker-compose.y[a]ml')
-    parser.add_argument('-v', '--volumes', action='count', default=0, help='list volumes')
+    parser.add_argument('-v', '--volumes', action='count', default=0,
+                        help='list volumes (use -vv to also list content)')
     parser.add_argument('-e', '--envs', action='store_true', help='list environment variables')
     parser.add_argument('-b', '--build', action='store_true', help='output build context and arguments')
     parser.add_argument('-p', '--path', action='store_true', help='print path of compose file')
+    parser.add_argument('-a', '--all', action='store_true', help='print everything')
     args = parser.parse_args()
 
     for project in args.projects:
         print_project(project,
-                      print_path=args.path,
-                      output_build=args.build,
-                      list_volumes=args.volumes,
-                      list_environment=args.envs,
+                      print_path=args.all or args.path,
+                      output_build=args.all or args.build,
+                      list_volumes=2 if args.all else args.volumes,
+                      list_environment=args.all or args.envs,
                       )
 
     return 0
