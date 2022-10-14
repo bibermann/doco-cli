125751f9fed3d5b15589d0b91ab50725cdbd29e3
Fabian Sandoval Saldias <fabianvss@gmail.com>
2022-10-14 15:50:04 +0200

    Rename getbackup command to get-backup

--- doco.py
+++ doco.py
@@ -9,7 +9,7 @@ import argcomplete
 
 import commands.backup
 import commands.down
-import commands.getbackup
+import commands.get_backup
 import commands.log
 import commands.restart
 import commands.restore
@@ -23,7 +23,7 @@ DOWN_COMMAND = 'd'
 UP_COMMAND = 'u'
 LOG_COMMAND = 'l'
 BACKUP_COMMAND = 'backup'
-GET_BACKUP_COMMAND = 'getbackup'
+GET_BACKUP_COMMAND = 'get-backup'
 RESTORE_COMMAND = 'restore'
 
 
@@ -48,7 +48,7 @@ def main() -> int:
     commands.up.add_to_parser(subparsers.add_parser(UP_COMMAND, help='up'))
     commands.log.add_to_parser(subparsers.add_parser(LOG_COMMAND, help='log'))
     commands.backup.add_to_parser(subparsers.add_parser(BACKUP_COMMAND, help='backup'))
-    commands.getbackup.add_to_parser(
+    commands.get_backup.add_to_parser(
         subparsers.add_parser(GET_BACKUP_COMMAND, help='get backup for local analysis'))
     commands.restore.add_to_parser(subparsers.add_parser(RESTORE_COMMAND, help='restore a backup'))
 
@@ -68,7 +68,7 @@ def main() -> int:
     elif args.command == BACKUP_COMMAND:
         commands.backup.main(args)
     elif args.command == GET_BACKUP_COMMAND:
-        commands.getbackup.main(args)
+        commands.get_backup.main(args)
     elif args.command == RESTORE_COMMAND:
         commands.restore.main(args)
 
