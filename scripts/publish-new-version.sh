b495dca56ba5bd2077e6e9f7dd95bff6c528b318
Fabian Sandoval Saldias <fabianvss@gmail.com>
2022-11-03 20:29:09 +0100

    Lift bump-version.sh up to publish-new-version.sh

--- scripts/publish-new-version.sh
+++ scripts/publish-new-version.sh
@@ -3,12 +3,31 @@ set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
-eval $(poetry run bumpver show --no-fetch --env)
-OLD_VERSION="$CURRENT_VERSION"
+if ! [[ "$*" =~ ^(-p|--patch|-m|--minor|--major)$ ]]; then
+  echo >&2 "Usage: $0 -p|--patch|-m|--minor|--major"
+  exit 1
+fi
+
+# build early to catch errors
+#*****************************
+
+poetry build
+
+# run bumpver
+#*************
+
+get_current_version() {
+  eval $(poetry run bumpver show --no-fetch --env)
+  echo "$CURRENT_VERSION"
+}
+
+OLD_VERSION="$(get_current_version)"
 
 poetry run bumpver update --no-fetch "$@"
+CURRENT_VERSION="$(get_current_version)"
 
-eval $(poetry run bumpver show --no-fetch --env)
+# update CHANGELOG.md
+#*********************
 
 OLD_HEADER_LINE="## \[Unreleased\]"
 NEW_HEADER_LINE="## \[$CURRENT_VERSION\] -- $(date --iso)"
@@ -19,6 +38,10 @@ NEW_UNRELEASED_LINE="\[Unreleased\]: \1\/$CURRENT_VERSION...HEAD"
 NEW_RELEASE_LINE="\[$CURRENT_VERSION\]: \1\/$OLD_VERSION...$CURRENT_VERSION"
 sed -E "s/^$OLD_UNRELEASED_LINE\$/$NEW_UNRELEASED_LINE\n$NEW_RELEASE_LINE/" -i CHANGELOG.md
 
-git commit --amend --no-edit -- CHANGELOG.md
+# amend commit, tag and publish
+#*******************************
 
-git tag -a -m "$CURRENT_VERSION" "$CURRENT_VERSION"
+git commit --amend --no-edit -- CHANGELOG.md
+git tag -a -m "Release version $CURRENT_VERSION" "v$CURRENT_VERSION"
+
+poetry publish --build
