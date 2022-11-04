9532759e67d7ae48bf110b331e1bf03284744a3d
Fabian Sandoval Saldias <fabianvss@gmail.com>
2022-11-04 14:00:53 +0100

    Slightly adjust help texts

--- src/commands/down.py
+++ src/commands/down.py
@@ -43,10 +43,10 @@ def down_project(project: ComposeProject, options: Options, info: ProjectInfo):
 def main(
     projects: list[pathlib.Path] = PROJECTS_ARGUMENT,
     running: bool = RUNNING_OPTION,
-    remove_volumes: bool = typer.Option(False, "--remove-volumes", "-v", help="Remove volumes (adds -v)."),
-    no_remove_orphans: bool = typer.Option(
-        False, "--no-remove-orphans", "-k", help="Keep orphans (omits --remove-orphans)."
+    remove_volumes: bool = typer.Option(
+        False, "--remove-volumes", "-v", help="Remove volumes (implies -f / --force)."
     ),
+    no_remove_orphans: bool = typer.Option(False, "--no-remove-orphans", "-k", help="Keep orphans."),
     force: bool = typer.Option(False, "--force", "-f", help="Force calling down even if not running."),
     dry_run: bool = typer.Option(
         False, "--dry-run", "-n", help="Do not actually stop anything, only show what would be done."
